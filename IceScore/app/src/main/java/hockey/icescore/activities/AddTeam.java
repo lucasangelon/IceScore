9d13d502027e62bad7fcc190f13628700e216997
Urist McPCMR <jack.kitchener-work@mail.com>
2015-05-12 23:24:20 +0800

    Team edit functionality
    create player
    search player
    add to team
    remove from team

--- IceScore/app/src/main/java/hockey/icescore/activities/AddTeam.java
+++ IceScore/app/src/main/java/hockey/icescore/activities/AddTeam.java
@@ -1,23 +1,44 @@
 package hockey.icescore.activities;
-
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import hockey.icescore.OldClasses.*;
 import hockey.icescore.OldClasses.Game;
 import hockey.icescore.R;
+import hockey.icescore.fragments.CreatePlayer;
 import hockey.icescore.util.Fragment_Listener;
+
+import static hockey.icescore.OldClasses.Game.awayTeam;
+import static hockey.icescore.OldClasses.Game.homeTeam;
 
 /**
  * Created by Lucas Angelon on 21-Mar-15.
  */
-public class AddTeam extends ActionBarActivity implements Fragment_Listener
-{
+public class AddTeam extends ActionBarActivity implements Fragment_Listener, View.OnClickListener, AdapterView.OnItemClickListener {
+
+    ListView current;
+    ListView searchview;
+    EditText playerSearch ;
+    Team team;
+    String homexaway;
+    int selected;
+    int searchSelected;
+
+   public static ArrayList<Player> spares = new ArrayList<Player>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +46,35 @@ public class AddTeam extends ActionBarActivity implements Fragment_Listener
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         String newString;
+        //temp
+        spares.add(new Player(55,"James Osborne",30));
+        spares.add(new Player(56,"ImmaBmo",32));
+        spares.add(new Player(57,"Gus",56));
+        spares.add(new Player(58,"Saruchi",18));
+        spares.add(new Player(55,"Shaye Trutwin",68));
+
+
+        searchview =(ListView) findViewById(R.id.searchList);
+        searchview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                searchSelected = position;
+            }
+        });
+
+        ArrayAdapter<Player> arrayAdapter = new ArrayAdapter<Player>(
+                this,
+                android.R.layout.simple_list_item_1,
+                spares);
+        searchview.setAdapter(arrayAdapter);
+
+        current =(ListView) findViewById(R.id.currentPlayersList);
+        current.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                selected=position;
+            }
+        });
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if(extras == null) {
@@ -35,25 +85,46 @@ public class AddTeam extends ActionBarActivity implements Fragment_Listener
         } else {
             newString= (String) savedInstanceState.getSerializable("team");
         }
-        ListView current =(ListView) findViewById(R.id.currentPlayersList);
+        homexaway = newString;
+        setCurrent();
 
-        switch(newString){
-            case "home":
-                ArrayAdapter<Player> arrayAdapter = new ArrayAdapter<Player>(
-                        this,
-                        android.R.layout.simple_list_item_1,
-                        Game.homeTeam.players );
-                current.setAdapter(arrayAdapter);
-            break;
-            case "away":
-                ArrayAdapter<Player> arrayAdapter1 = new ArrayAdapter<Player>(
-                        this,
-                        android.R.layout.simple_list_item_1,
-                        Game.awayTeam.players );
-                current.setAdapter(arrayAdapter1);
-                break;
+        Button delete = (Button) findViewById(R.id.deleteplayerbutton);
+        delete.setOnClickListener(this);
 
-        }
+        Button add = (Button) findViewById(R.id.addplayerbutton);
+        add.setOnClickListener(this);
+
+        Button create = (Button) findViewById(R.id.createPlayerButton);
+        create.setOnClickListener(this);
+
+
+
+       playerSearch = (EditText) findViewById(R.id.playersearch);
+        playerSearch.addTextChangedListener(new TextWatcher() {
+            ArrayList<Player> temp;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+             temp = new ArrayList<Player>();
+                int i = 0;
+                while (i<spares.size()) {
+                    if(spares.get(i).name.trim().contains(playerSearch.getText().toString().trim()))
+                        temp.add(spares.get(i));
+                    i++;
+                }
+                refreshSearch(temp);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
@@ -63,6 +134,48 @@ public class AddTeam extends ActionBarActivity implements Fragment_Listener
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_general, menu);
         return true;
+    }
+    public void refreshSearch(ArrayList<Player> temp)
+    {
+                ArrayAdapter<Player> arrayAdapter3 = new ArrayAdapter<Player>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        temp);
+                searchview.setAdapter(arrayAdapter3);
+    }
+
+    public void setCurrent()
+    {
+        switch(homexaway){
+            case "home":
+                ArrayAdapter<Player> arrayAdapter = new ArrayAdapter<Player>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        homeTeam.players );
+                current.setAdapter(arrayAdapter);
+                this.team = homeTeam;
+                ArrayAdapter<Player> arrayAdapter3 = new ArrayAdapter<Player>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        spares);
+                searchview.setAdapter(arrayAdapter3);
+                break;
+            case "away":
+                ArrayAdapter<Player> arrayAdapter1 = new ArrayAdapter<Player>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        awayTeam.players );
+                current.setAdapter(arrayAdapter1);
+                this.team = awayTeam;
+                ArrayAdapter<Player> arrayAdapter4 = new ArrayAdapter<Player>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        spares);
+                searchview.setAdapter(arrayAdapter4);
+                break;
+
+
+        }
     }
 
     @Override
@@ -77,6 +190,68 @@ public class AddTeam extends ActionBarActivity implements Fragment_Listener
 
     @Override
     public void buttonClicked(String val) {
+
+    }
+
+    public void removePlayer()
+    {
+         if(team.getTeamID() == homeTeam.getTeamID())
+         {
+             spares.add(homeTeam.getPlayer(selected));
+             homeTeam.players.remove(selected);
+         }
+         else
+         {
+             spares.add(homeTeam.getPlayer(selected));
+             awayTeam.players.remove(selected);
+         }
+    }
+
+    public void addPlayer()
+    {
+        if(team.getTeamID() == homeTeam.getTeamID())
+        {
+            homeTeam.players.add(spares.get(searchSelected));
+            spares.remove(spares.get(searchSelected));
+        }
+        else
+        {
+            awayTeam.players.add(spares.get(searchSelected));
+            spares.remove(spares.get(searchSelected));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.deleteplayerbutton:
+                removePlayer();
+                setCurrent();
+                break;
+
+            case R.id.addplayerbutton:
+                addPlayer();
+                setCurrent();
+                break;
+
+            case R.id.createPlayerButton:
+                CreatePlayer p1 = new CreatePlayer();
+                android.app.FragmentManager manager1=getFragmentManager();
+                android.app.FragmentTransaction transaction1=manager1.beginTransaction();
+                transaction1.add(android.R.id.content, p1, "Create player");
+                transaction1.commit();
+                break;
+
+
+        }
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this,"you selected "+current.getAdapter().getItem(position),Toast.LENGTH_SHORT).show();
 
     }
 }
