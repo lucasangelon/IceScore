340f935d33c0bd97254d618908286c3cbc0dc033
SUCHI <bapat.suruchi@gmail.com>
2015-05-19 11:38:50 +0800

    Navigation and Action Bar Heading Standardised
    Navigation and Action bar standardisation applied to most of the
    screens.
    
    Game Log
    Injury
    Game Select

--- IceScore/app/src/main/java/hockey/icescore/activities/Injury.java
+++ IceScore/app/src/main/java/hockey/icescore/activities/Injury.java
@@ -92,6 +92,11 @@ public class Injury extends ActionBarActivity implements Fragment_Listener, View
         //InsertIntoDb(new Injury(currentTeam, Game.currentPeriod, name, desc.gtText()));
     }
 
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
 
     @Override
     public void onClick(View v) {
