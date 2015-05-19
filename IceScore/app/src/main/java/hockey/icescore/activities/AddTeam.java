28da61b96562150c32e6975bc521232bb247492b
SUCHI <bapat.suruchi@gmail.com>
2015-05-19 11:44:23 +0800

    Added the Back functionality to Add Team Screen
    Added Back Button click to Add Team Screen

--- IceScore/app/src/main/java/hockey/icescore/activities/AddTeam.java
+++ IceScore/app/src/main/java/hockey/icescore/activities/AddTeam.java
@@ -220,7 +220,11 @@ public class AddTeam extends ActionBarActivity implements Fragment_Listener, Vie
             spares.remove(spares.get(searchSelected));
         }
     }
-
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
     @Override
     public void onClick(View v) {
 
